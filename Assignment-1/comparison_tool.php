f94333fe1c52625662a05688f99edc9f15a4078d
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-11 19:36:44 -0400

    Add comparision_tool

--- Assignment-1/comparison_tool.php
+++ Assignment-1/comparison_tool.php
@@ -7,5 +7,10 @@ Create a form where the user can input two numbers.
 Use the ternary operator to determine the larger number and display the result.
 */
 
+$firstNumber = (float)readline("Enter first number : ");
+$secondNumber = (float)readline("Enter second number : ");
 
+$largeNumber = ($firstNumber > $secondNumber)? $firstNumber : $secondNumber;
+
+echo "Larger number is : $largeNumber";
 ?>
