cbb2512b8c0ac76c6d711025a8bc5eeb504ef8d5
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-13 00:19:34 -0400

    Added grade Calculator

--- Assignment-1/grade_calculator.php
+++ Assignment-1/grade_calculator.php
@@ -8,5 +8,25 @@ Create a form where the user can input three test scores.
 Calculate the average and display it along with the corresponding grade (A, B, C, D, F).
 */
 
+$mark = readline("Enter your mark : ");
 
-?>
+if ($mark > 100) {
+    echo "Your mark can't be more than 100";
+} else {
+    switch ($mark) {
+        case ($mark >= 90):
+            echo "Your grade is A";
+            break;
+        case ($mark >= 70 && $mark <= 89):
+            echo "Your grade is B";
+            break;
+        case ($mark >= 50 && $mark <= 69):
+            echo "Your grade is C";
+            break;
+        case ($mark >= 33 && $mark <= 49):
+            echo "Your grade is D";
+            break;
+        default:
+            echo "Your grade is F";
+    }
+}
