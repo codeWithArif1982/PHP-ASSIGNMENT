9593bd740f36ff1a5364f6cbaeb093e0d8ca2921
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-26 19:32:28 -0400

    task_1 code is optimized

--- Assignment-3/task_1.php
+++ Assignment-3/task_1.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Task 1: String Manipulation
 Create a string variable called $text with the value "The quick brown fox jumps over the lazy dog.". Write a PHP function which takes "$text" as an argument to:
@@ -10,15 +10,18 @@ Print the modified text.
 
 $text = "The quick brown fox jumps over the lazy dog.";
 
-function modifyText($inputText) {
+function modifyText($inputText)
+{
     // Convert the string to all lowercase
     $lowercaseText = strtolower($inputText);
-    
+
     // Replace "brown" with "red" in the string
     $modifiedText = str_replace("brown", "red", $lowercaseText);
-    
+
+    // Print the Original text
+    echo "Original Text : " . $inputText . "\n";
     // Print the modified text
-    echo $modifiedText;
+    echo "Modified Text brown replace with red : " . $modifiedText;
 }
 
 // Call the function with the provided text
