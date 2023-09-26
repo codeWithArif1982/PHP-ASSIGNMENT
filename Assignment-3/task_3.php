0191af4860f6781d370f186c90d82ce35b623b11
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-26 19:36:39 -0400

    Assignment - 3: task_3 added

--- Assignment-3/task_3.php
+++ Assignment-3/task_3.php
@@ -1,6 +1,20 @@
-<?php 
+<?php
 /*
 Task 3: Array Sorting  
 Create an array called $grades with the following values: 85, 92, 78, 88, 95.
 Write a PHP function which takes "$grades" as an argument to sort the array in descending order and print the sorted grades as array.
 */
+
+
+// Create an array called $grades with the given values
+$grades = array(85, 92, 78, 88, 95);
+
+function sortGradesDescending($gradesArray)
+{
+    // Sort the array in descending order
+    arsort($gradesArray);
+    // Print the sorted grades as an array
+    print_r($gradesArray);
+}
+// Call the function with the $grades array
+sortGradesDescending($grades);
