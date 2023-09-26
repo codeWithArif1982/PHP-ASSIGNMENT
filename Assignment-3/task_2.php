b2ec6639e8e04cd38bec7f62f3244ac0d34bd8c3
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-26 19:34:07 -0400

    Assignment -3: task_2 added

--- Assignment-3/task_2.php
+++ Assignment-3/task_2.php
@@ -1,6 +1,23 @@
-<?php 
+<?php
 /*
 Task 2: Array Manipulation
 Create an array called $numbers containing the numbers 1 to 10.
 Write a PHP function which takes the "$numbers" array as an argument to remove the even numbers from the array and print the resulting array.
 */
+
+// Create an array called $numbers containing the numbers 1 to 10
+$numbers = range(1, 10);
+
+function removeEvenNumbers($arr)
+{
+    // Use array_filter to remove even numbers from the array
+    $filteredArray = array_filter($arr, function ($number) {
+        return $number % 2 != 0;
+    });
+
+    // Print the resulting array
+    print_r($filteredArray);
+}
+
+// Call the function with the $numbers array
+removeEvenNumbers($numbers);
